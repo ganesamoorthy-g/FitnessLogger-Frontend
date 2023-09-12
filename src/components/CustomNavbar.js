688fb2559dd2971f4d5c46158a01ab00bac92d00
ganesamoorthy-g <gurunathganesh6@gmail.com>
2023-09-12 19:32:53 +0530

    final1

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function CustomNavbar() {
   return (
     <Navbar bg="transperant" variant="transperant" className="custom-navbar sticky-top">
       <Navbar.Brand>Fitness Logger</Navbar.Brand>
       <Nav className="ml-auto custom-nav-links">
-        <Nav.Link href="/allexcercisepage">Add Exercise</Nav.Link>
-        <Nav.Link href="/history">History</Nav.Link>
-        <Nav.Link href="/">Logout</Nav.Link>
+        <Link to="/allexcercisepage">Add Exercise</Link>
+        <Link to="/history">History</Link>
+        <Link to="/">Logout</Link>
       </Nav>
     </Navbar>
   );
